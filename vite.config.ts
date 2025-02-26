5af5debdb3c9c7b43f29ef7f5d4b4d01b2e9051a
Francisco Gama Franco Soares Martins <147647438+inaciobalu@users.noreply.github.com>
2025-02-26 03:53:25 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +7,4 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/CV-Website/',  // Add this line to specify the base URL for GitHub Pages
-})
+});
