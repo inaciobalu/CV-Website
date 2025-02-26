1b2745dcc358434499afbb3de79f0837c3d33e5d
Francisco Gama Franco Soares Martins <147647438+inaciobalu@users.noreply.github.com>
2025-02-26 04:01:54 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/CV-Website/',  // Add this line to set the base path for GitHub Pages
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
